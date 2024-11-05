79065091c612b16602934f69db9bc486469e141a
andrezamatsuoka <120607732+andrezamatsuoka@users.noreply.github.com>
2024-11-05 11:30:54 -0300

    Add files via upload

---
+++ controller/InterfaceController.java
@@ -0,0 +1,48 @@
+package controller;
+import model.*;
+import view.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class InterfaceController extends InterfaceView {
+    public static final String localViewImgFolder = System.getProperty("user.dir") 
+        + "\\" 
+        + "src"
+        + "\\" 
+        + "view"
+        + "\\"
+        + "img";
+
+    public static final String localViewFolder = System.getProperty("user.dir") 
+        + "\\" 
+        + "src"
+        + "\\" 
+        + "view";
+
+    public static final Icon imgPadrao = new ImageIcon(new ImageIcon(localViewFolder + "\\imagem-padrao.jpg").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
+
+    public static void verificarApagarImagensInuteis() {
+        final File folder = new File(localViewImgFolder);
+        ArrayList<String> strImagens = listFilesForFolder(folder);
+        InterfaceModel.validarImagens(strImagens);
+    }
+
+    public static ArrayList<String> listFilesForFolder(final File folder) {
+        ArrayList<String> strFiles = new ArrayList<String>();
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                strFiles.add(fileEntry.getName());
+                // System.out.println(fileEntry.getName());
+            }
+        }
+        return strFiles;
+    }
+
+    public static String gerarNomeAleatorio() {
+        return String.format("file-%s", Math.random());
+    }
+}
